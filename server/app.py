8d3947cfc064d3657cf83261c5d60f92b6c62a3c
Guang <jouyang19@proton.me>
2024-07-11 09:27:41 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,21 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    return [a.to_dict() for a in articles]
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    article = Article.query.filter(Article.id == id).first()
+    session['page_views'] = session.get("page_views") or 0
+    session['page_views'] += 1
+    if session['page_views'] > 3:
+        response_dict = {'message':'Maximum pageview limit reached'}
+        response = make_response(response_dict, 401)
+        return response
+    response_dict = article.to_dict()
+    response = make_response(response_dict, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
